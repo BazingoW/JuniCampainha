f4ba9db84b1354497556c221e8dde39e021eb9d9
BazingoW <ramiro.animus@gmail.com>
2017-10-27 00:35:10 +0100

    codigo agora so faz um http request via curl

--- C program/main.c
+++ C program/main.c
@@ -10,8 +10,8 @@ int main()
     char buff[512];
     int id=6;
     int len;
-    char str[200]= "curl -s http://web.tecnico.ulisboa.pt/ist181138/Junitec/isdingdong.php/";
-    char str2[200]  = "curl -s http://web.tecnico.ulisboa.pt/ist181138/Junitec/dingdongoff.php/";
+    char str[200]= "curl -s http://web.tecnico.ulisboa.pt/ist181138/Junitec/Bell/checkCampainha.php";
+
 
 
     while (1) {
@@ -22,16 +22,13 @@ int main()
        }
 
       while(fgets(buff, sizeof(buff), in)!=NULL)
-       {
-           //printf("%s\n",buff);
-
-
-
-      }
+       {}
       pclose(in);
 
+      //if page says true
       if (strstr(buff, "true") != NULL) {
 
+      //do a DingDong
       printf("\aDingDong!\n");
       usleep(500000);  // sleep(0.5s)
       //sleep(1);
@@ -41,27 +38,6 @@ int main()
       printf("\a\n");
       sleep(1);
       printf("\a\n");
-
-      if(!(in = popen(str2, "r")))
-       {
-          return 0;
-       }
-
-      while(fgets(buff, sizeof(buff), in)!=NULL)
-       {
-          // printf("%s\n",buff);
-
-
-
-      }
-      pclose(in);
-      }
-
-    sleep(1);
     }
-
-
-
-
-
+ }
  }
